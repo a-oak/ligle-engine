2efaf621f03d44a557159e2371d0cb8a2355a13b
crackhopper <lixiangalixiang@gmail.com>
2015-08-06 18:33:33 +0800

    add hooks for plugin to use.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var ligle={};
 ligle.util = require('ligle-util');
 var logger = ligle.util.logger('ligle-engine','TRACE');
 var configure = ligle.util.configure;
+var async = require('async');
 
 var defaultCfg = {
   'ligle-model':{
@@ -26,10 +27,58 @@ module.exports = function(config){
   exportObj={};
   exportObj.util = require('ligle-util');
   exportObj.base = require('ligle-base')(cfg);
-  exportObj.start = exportObj.base.start.bind(exportObj.base);
+  // hooks of engine
+  exportObj.initHooks = [];
+  exportObj.dbOpenedHooks = [];
+  exportObj.exitHooks = [];
+  exportObj.runHooks = function(hooks,done){
+    async.parallel(hooks,function(err,result){
+      done(err,result);
+    });
+  };
 
-// we now purge out service, and in future would add this module.
-//  exportObj.service = require('ligle-service');
+  var self = exportObj;
+  function exitHandler(err) {
+    self.runHooks(self.exitHooks,function(err,result){
+      logger.info('exit hooks finished');
+      if(err) console.log(err.stack);
+      self.base.db.close(function(){
+        process.nextTick(function(){
+          logger.info('db closed');
+          process.exit(0);
+        });
+      });
+    });
+  };
+  var _afterhook = function(){
+    //catches ctrl+c event
+    process.on('SIGINT', exitHandler);
+    //catches uncaught exceptions
+    process.on('uncaughtException', exitHandler);
+  };
+  var _start = function(runApp){
+    self.base.start(function(){// opendb
+      self.runHooks(self.dbOpenedHooks,function(err,result){
+        logger.info('dbOpened hooks finished');
+
+        // run user logic
+        runApp();
+        
+        // add exit hooks
+        _afterhook();
+      });
+    });
+  };
+
+  exportObj.start = function(runApp){    
+    this.runHooks(this.initHooks,function(err,result){
+      logger.info('init hooks finished');
+      _start(runApp);
+    });
+
+    // we now purge out service, and in future would add this module.
+    //  exportObj.service = require('ligle-service');
+  };
 
   logger.trace('configuration:',cfg);
   return exportObj;
